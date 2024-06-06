737411f45d4002c9ed03658b3d3c325602416cf3
오다혜 <fffvvv6@gmail.com>
2024-06-07 07:25:39 +0900

    feat: ZERO 텍스트 추가

--- dahye/week1/src/components/Title.tsx
+++ dahye/week1/src/components/Title.tsx
@@ -3,11 +3,19 @@ import styles from './Title.module.css';
 
 const Title = () => {
   return (
-    <>
-      <div className={styles['ball-container']}>
-        <Ball />
+    <div className={styles['title-container']}>
+      <div className={styles['content-container']}>
+        <div className={styles['ball-container']}>
+          <Ball />
+        </div>
+        <div className={styles['zero-container']}>
+          <span>Z</span>
+          <span>E</span>
+          <span>R</span>
+          <span>O</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
