de41b4fd4db1015b186c038f419da1238b207557
오다혜 <fffvvv6@gmail.com>
2024-06-22 03:29:25 +0900

    feat: 마우스 움직이는 위치에 따라서 에셋 돌게 하기

--- dahye/week3/src/components/Model.tsx
+++ dahye/week3/src/components/Model.tsx
@@ -1,6 +1,12 @@
-import { useRef } from 'react';
-import { MeshTransmissionMaterial, useGLTF, Text } from '@react-three/drei';
-import { useFrame, useThree } from '@react-three/fiber';
+import { useRef, useState } from 'react';
+import {
+  MeshTransmissionMaterial,
+  useGLTF,
+  Text,
+  Draggable,
+} from '@react-three/drei';
+import { ThreeEvent, useThree } from '@react-three/fiber';
+
 import { useControls } from 'leva';
 import { Mesh } from 'three';
 
@@ -9,11 +15,43 @@ export default function Model() {
   const { viewport } = useThree();
   const torus = useRef<Mesh>(null);
 
-  useFrame(() => {
-    if (!torus.current) return;
-
-    torus.current.rotation.x += 0.02;
+  const [isDragging, setIsDragging] = useState(false);
+  const [previousMousePosition, setPreviousMousePosition] = useState({
+    x: 0,
+    y: 0,
   });
+
+  const handleMouseDown = (event: ThreeEvent<PointerEvent>) => {
+    event.stopPropagation();
+    setIsDragging(true);
+    setPreviousMousePosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
+
+  const handleMouseMove = (event: ThreeEvent<PointerEvent>) => {
+    if (!isDragging) return;
+
+    const { clientX, clientY } = event;
+
+    const deltaX = clientX - previousMousePosition.x;
+    const deltaY = clientY - previousMousePosition.y;
+
+    if (torus.current === null) return;
+
+    torus.current.rotation.y += deltaX * 0.01;
+    torus.current.rotation.x += deltaY * 0.01;
+
+    setPreviousMousePosition({
+      x: clientX,
+      y: clientY,
+    });
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
 
   const materialProps = useControls({
     thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
@@ -35,7 +73,14 @@ export default function Model() {
       >
         hello world!
       </Text>
-      <mesh ref={torus} {...nodes.Torus002}>
+
+      <mesh
+        ref={torus}
+        {...nodes.Torus002}
+        onPointerDown={handleMouseDown}
+        onPointerMove={handleMouseMove}
+        onPointerUp={handleMouseUp}
+      >
         <MeshTransmissionMaterial {...materialProps} />
       </mesh>
     </group>
